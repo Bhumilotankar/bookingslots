e0975d14259da62e541332f4bb0448fa62009218
Aayush <co2021.aayush.balip@ves.ac.in>
2023-07-03 16:17:29 +0530

    Styles

---
+++ style.js
@@ -0,0 +1,55 @@
+import { StyleSheet } from 'react-native'
+
+const Bynocs_work = StyleSheet.create({
+    view:{
+        color:'black'
+    },
+    text:{
+        color:'black'
+    }
+})
+
+const Patient_say = StyleSheet.create({
+    view:{
+        color:'black'
+    },
+    text:{
+        color:'black'
+    }
+})
+
+const Doctor_say = StyleSheet.create({
+    view:{
+        color:'black'
+    },
+    text:{
+        color:'black'
+    }
+})
+
+const Bynocs_Research = StyleSheet.create({
+    view:{
+        color:'black'
+    },
+    text:{
+        color:'black'
+    }
+})
+
+const login_page = StyleSheet.create({
+    view:{
+        color:'black'
+    },
+    text:{
+        color:'black'
+    }
+})
+
+const enquiry = StyleSheet.create({
+    view:{
+        color:'black'
+    },
+    text:{
+        color:'black'
+    }
+})
