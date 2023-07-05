0f5ef80b0b734303cfa670bcaa38dcf9e187e545
Aayush <co2021.aayush.balip@ves.ac.in>
2023-07-05 23:33:35 +0530

    trial

--- screens/slides/work.js
+++ screens/slides/work.js
@@ -1,207 +1,159 @@
-import React, { useEffect, useState } from 'react';
-import { ImageBackground, StyleSheet, Text, View, TouchableOpacity, Image, ScrollView } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import YoutubePlayer from 'react-native-youtube-iframe';
+import { Text, TouchableOpacity, View, ImageBackground, Dimensions, ScrollView, Alert } from 'react-native';
 import Swiper from 'react-native-swiper';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons'
+import slide2_style from '../../styles/slide2';
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
-const Works = () => {
-  const [backgroundImage, setBackgroundImage] = useState(null);
-  const [longDesc, setLongDesc] = useState('');
-  const [shortDesc, setShortDesc] = useState('');
+const How_Bynocs_Work = ({ language, onLanguageChange }) => {
+  const [enable, setEnable] = useState(true);
+  const [APIData, setAPIData] = useState([]);
   const [showLongDesc, setShowLongDesc] = useState(false);
 
+  const { height, width } = Dimensions.get('window');
+
   useEffect(() => {
-    fetchData();
+    APIgetData();
   }, []);
-
-  const fetchData = async () => {
-    try {
-      const response = await fetch('https://www.myjsons.com/v/f29b1312');
-      const data = await response.json();
-      const imageUrl = data.items[0].en.howBynocsWork.image;
-      const longDescription = data.items[0].en.howBynocsWork.longdesc;
-      const shortDescription = data.items[0].en.howBynocsWork.shortdesc;
-      setBackgroundImage(imageUrl);
-      setLongDesc(longDescription);
-      setShortDesc(shortDescription);
-    } catch (error) {
-      console.log('Error fetching data:', error);
-    }
+  const APIgetData = async () => {
+    const response = await fetch('https://www.myjsons.com/v/f29b1312');
+    const jsonData = await response.json();
+    setAPIData(jsonData.items);
   };
-
-  const toggleLongDesc = () => {
+  const toggleDescription = () => {
     setShowLongDesc(!showLongDesc);
   };
 
-  const handleSkip = () => {
-    // Handle skip button action here
-    console.log('Skip button pressed');
+  const saveLanguage = (lang) => {
+    onLanguageChange(lang);
   };
 
   return (
-    <View style={styles.container}>
-      {backgroundImage && (
-        <ImageBackground source={{ uri: backgroundImage }} style={styles.imageBackground}>
-          <TouchableOpacity onPress={handleSkip} style={styles.skipButton}>
-            <Text style={styles.skipButtonText}>Skip</Text>
-          </TouchableOpacity>
-          <View style={styles.overlay}>
-            <Text style={styles.title}>How Bynocs Works</Text>
-            <ScrollView contentContainerStyle={styles.scrollView}>
-            {showLongDesc ? (
-              <Text style={styles.description}>{longDesc}</Text>
-            ) : (
-              <Text style={styles.shortDescription}>{shortDesc}</Text>
-            )}
-            </ScrollView>
-            <TouchableOpacity onPress={toggleLongDesc} style={styles.arrowContainer}>
-              <Image
-                source={showLongDesc ? require('./assets/up.jpeg') : require('./assets/down.jpeg')}
-                style={styles.arrowIcon}
-              />
+    <View style={{ height: height }}>
+      {APIData.map((item, key) => {
+        const bynocsData = item;
+
+       console.log(bynocsData.en.howBynocsWork.longdesc);
+
+        return (
+          <View key={key}>
+            <ImageBackground source={{ uri: bynocsData.en.howBynocsWork.image }} style={{ width: width, height: height, flex: 1 }} />
+            <View style={slide2_style.language_view}>
+              <TouchableOpacity onPress={() => saveLanguage('english')}>
+                <Text style={slide2_style.language_text}>EN</Text>
+              </TouchableOpacity>
+              <Text style={slide2_style.language_bar}> | </Text>
+              <TouchableOpacity onPress={() => saveLanguage('french')}>
+                <Text style={slide2_style.language_text}>FR</Text>
+              </TouchableOpacity>
+            </View>
+            <View style={slide2_style.Content_view}>
+              {language=='french' ? (
+                <Text style={{
+                  fontSize: 22,
+                  fontFamily: 'Poppins_bold',
+                  color: '#175ca4',
+                  fontWeight: 'bold',
+                  lineHeight: 50,
+                  alignSelf: 'center'
+                }}>{item.fr.howBynocsWork.title}</Text>
+              ) : (
+                <Text style={slide2_style.Content_title}>{bynocsData.en.howBynocsWork.title}</Text>
+              )}
+
+              {!showLongDesc ? (
+                language=='english' ? (
+                  <Text
+                    style={{
+                      fontSize: 16,
+                      marginLeft: 15,
+                      marginTop: 10,
+                      fontFamily: 'times new roman',
+                      color: 'black',
+                      lineHeight: 25,
+                    }}
+                  >
+                    {item.en.howBynocsWork.shortdesc}
+                  </Text>
+                ) : (
+                  <Text
+                    style={{
+                      fontSize: 16,
+                      marginLeft: 15,
+                      marginTop: 10,
+                      fontFamily: 'times new roman',
+                      color: 'black',
+                      lineHeight: 22,
+                    }}
+                  >
+                    {item.fr.howBynocsWork.shortdesc}
+                  </Text>
+                )
+
+              ) : (
+                language=='english' ? (
+                  <View style={{ flex: 1 }}>
+                    <ScrollView
+                      style={{
+                        marginLeft: 15,
+                        marginTop: 10,
+                      }}
+                    >
+                      <Text
+                        style={{
+                          fontSize: 16,
+                          fontFamily: 'times new roman',
+                          color: 'black',
+                          lineHeight: 25,
+                        }}
+                      >
+                        {item.en.howBynocsWork.longdesc}
+                      </Text>
+                    </ScrollView>
+                  </View>
+                ) : (
+                  <View style={{ flex: 1 }}>
+                    <ScrollView
+                      style={{
+                        marginLeft: 15,
+                        marginTop: 10,
+                      }}
+                    >
+                      <Text
+                        style={{
+                          fontSize: 16,
+                          fontFamily: 'times new roman',
+                          color: 'black',
+                          lineHeight: 22,
+                        }}
+                      >
+                        {item.fr.howBynocsWork.longdesc}
+                      </Text>
+                    </ScrollView>
+                  </View>
+                )
+
+              )}
+              <View style={{ alignItems: 'center' }}>
+                <MaterialIcons
+                  name={showLongDesc ? 'keyboard-arrow-up' : 'keyboard-arrow-down'}
+                  size={34}
+                  onPress={toggleDescription}
+                />
+              </View>
+            </View>
+            <TouchableOpacity activeOpacity={1} style={slide2_style.Touchable_Opacity}>
+              <Text style={slide2_style.Touchable_Text}>LOGIN</Text>
+            </TouchableOpacity>
+            <TouchableOpacity activeOpacity={1} style={slide2_style.Enquiry_button}>
+              <Text style={slide2_style.Touchable_Text}>ENQUIRY</Text>
             </TouchableOpacity>
           </View>
-          <View style={styles.buttonContainer}>
-            <TouchableOpacity style={styles.button}>
-              <Text style={styles.buttonText}>LOGIN</Text>
-            </TouchableOpacity>
-            <TouchableOpacity style={styles.button}>
-              <Text style={styles.buttonText}>ENQUIRY</Text>
-            </TouchableOpacity>
-          </View>
-          <Swiper
-            style={styles.carouselContainer}
-            dot={<View style={styles.dot} />}
-            activeDot={<View style={styles.activeDot} />}
-            paginationStyle={styles.pagination}
-          >
-            {/* Add your carousel slides here */}
-            <View style={styles.slide}>
-              <Text>Slide 1</Text>
-            </View>
-            <View style={styles.slide}>
-              <Text>Slide 2</Text>
-            </View>
-            <View style={styles.slide}>
-              <Text>Slide 3</Text>
-            </View>
-          </Swiper>
-        </ImageBackground>
-      )}
+        );
+      })}
     </View>
   );
 };
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  imageBackground: {
-    flex: 1,
-    resizeMode: 'cover',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  overlay: {
-    backgroundColor: '#ffff',
-    padding: 5,
-    borderRadius: 30,
-    alignItems: 'center',
-    marginTop: 360,
-    marginBottom:120,
-    marginLeft: 20,
-    marginRight: 20,
-    borderColor: '#175CA4',
-    borderWidth: 1,
-  },
-  skipButton: {
-    position: 'absolute',
-    top: 10,
-    right: 10,
-  },
-  skipButtonText: {
-    color: 'black',
-    fontSize: 16,
-  },
-  title: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    color: '#175CA4',
-    marginBottom: 10,
-  },
-  shortDescription: {
-    fontSize: 16,
-    color: 'black',
-    textAlign: 'center',
-  },
-  description: {
-    fontSize: 16,
-    color: 'black',
-    textAlign: 'center',
-    marginBottom: 20,
-  },
-  arrowContainer: {
-    marginTop: 10,
-  },
-  arrowIcon: {
-    width: 20,
-    height: 20,
-  },
-  buttonContainer: {
-    position: 'absolute',
-    bottom: 20,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    paddingHorizontal: 10,
-    width: '100%',
-    alignItems: 'center',
-  },
-  button: {
-    width: 161,
-    height: 50,
-    backgroundColor: '#175CA4',
-    borderRadius: 30,
-    justifyContent: 'center',
-    alignItems: 'center',
-    marginHorizontal: 5,
-  },
-  buttonText: {
-    color: '#ffffff',
-    fontSize: 16,
-  },
-  carouselContainer: {
-    position: 'absolute',
-    bottom: 70, // Adjust this value to set the position of the carousel
-    width: '100%',
-    height: 50, // Adjust this value to set the height of the carousel
-  },
-  slide: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  dot: {
-    width: 5,
-    height: 5,
-    borderRadius: 4,
-    backgroundColor: 'rgba(23, 92, 164, 0.48)',
-    backgroundColor: '#9DC2E9',
-    marginLeft: 3,
-    marginRight: 3,
-    marginTop: 3,
-    marginBottom: 3,
-  },
-  activeDot: {
-    width: 17,
-    height: 5,
-    borderRadius: 5,
-    backgroundColor: '#175CA4',
-    marginLeft: 3,
-    marginRight: 3,
-    marginTop: 3,
-    marginBottom: 3,
-  },
-  pagination: {
-    bottom:90,
-  },
-});
-
-export default Works;
+export default How_Bynocs_Work;
