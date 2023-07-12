31fdce1a972c68ca88fca67ddd860e8fb3476cd3
Aayush <co2021.aayush.balip@ves.ac.in>
2023-07-12 13:41:55 +0530

    Blogs n research UI

--- screens/slides/blogsAndResearch.js
+++ screens/slides/blogsAndResearch.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import { View, Image, TouchableOpacity, Text, ScrollView, Linking, Dimensions } from 'react-native';
+import { responsiveFontSize, responsiveHeight, responsiveWidth } from 'react-native-responsive-dimensions'
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import styles from '../../styles/styles';
 
 const BlogsAndResearch = ({ language, onLanguageChange, navigation }) => {
 const { height, width } = Dimensions.get('window');
@@ -31,96 +33,43 @@ const { height, width } = Dimensions.get('window');
     <View>
       {apiData.map((item, index) => (
         <View key={index}>
-          <View style={{ flexDirection: 'row', justifyContent: 'center', marginBottom: 10, position: 'absolute', top: 20, width: '100%', zIndex: 1 }}>
-            <TouchableOpacity onPress={() => saveLanguage('english')}>
-              <Text style={{ color: language === 'english' ? '#175ca4' : 'black', marginRight: 10, fontSize: 20 }}>English</Text>
-            </TouchableOpacity>
-            <TouchableOpacity onPress={() => saveLanguage('french')}>
-              <Text style={{ color: language === 'french' ? '#175ca4' : 'black', fontSize: 20 }}>French</Text>
-            </TouchableOpacity>
-          </View>
+          <View style={{ flexDirection: 'row', marginLeft: responsiveWidth(74), position: 'absolute', top: responsiveHeight(3), width: width, zIndex: 1 }}>
+              <TouchableOpacity onPress={() => saveLanguage('english')}>
+                <Text style={{ color: language === 'english' ? '#175ca4' : 'black', marginRight: responsiveWidth(0.6), fontSize: responsiveFontSize(3), fontFamily: 'Poppins-Regular' }}>EN</Text>
+              </TouchableOpacity>
+              <Text style={{ color: 'black', marginRight: responsiveWidth(0.6), fontSize: responsiveFontSize(3), fontFamily: 'Poppins-Regular' }}> | </Text>
+              <TouchableOpacity onPress={() => saveLanguage('french')}>
+                <Text style={{ color: language === 'french' ? '#175ca4' : 'black', fontSize: responsiveFontSize(3), fontFamily: 'Poppins-Regular' }}>FR</Text>
+              </TouchableOpacity>
+            </View>
           <Image
             source={{ uri: item.en.blogsAndResearch.backimage }}
             style={{ width: '100%', height: 812 }}
           />
 
           <View
-            style={{
-              borderRadius: 31,
-              backgroundColor: '#fff',
-              borderStyle: 'solid',
-              borderColor: '#175ca4',
-              borderWidth: 1,
-              flex: 1,
-              width: 312,
-              height: 265,
-              position: 'absolute',
-              marginLeft: 26,
-              marginRight: 20,
-              marginTop: 310,
-              marginBottom: 134,
-            }}
+            style={styles.Blogs_View}
           >
             {language === 'french' ? (
-              <Text style={{
-                fontSize: 28,
-                fontFamily: 'Poppins_bold',
-                color: '#175ca4',
-                fontWeight: 'bold',
-                lineHeight: 50,
-                alignSelf: 'center'
-              }}>{item.fr.blogsAndResearch.title}</Text>
+              <Text style={styles.Blogs_fr_title}>{item.fr.blogsAndResearch.title}</Text>
             ) : (
-              <Text style={{
-                fontSize: 28,
-                fontFamily: 'Poppins_bold',
-                color: '#175ca4',
-                fontWeight: 'bold',
-                lineHeight: 50,
-                alignSelf: 'center'
-              }}>{item.en.blogsAndResearch.title}</Text>
+              <Text style={styles.Blogs_en_title}>{item.en.blogsAndResearch.title}</Text>
             )}
             {language === 'french' ? (
-              <Text style={{
-                fontSize: 18,
-                fontFamily: 'Poppins_bold',
-                color: '#175ca4',
-                fontWeight: 'bold',
-                alignSelf: 'center'
-              }}>{item.fr.blogsAndResearch.contentTitle}</Text>
+              <Text style={styles.Blogs_fr_Content_title}>{item.fr.blogsAndResearch.contentTitle}</Text>
             ) : (
-              <Text style={{
-                fontSize: 18,
-                fontFamily: 'Poppins_bold',
-                color: '#175ca4',
-                fontWeight: 'bold',
-                alignSelf: 'center'
-              }}>{item.en.blogsAndResearch.contentTitle}</Text>
+              <Text style={styles.Blogs_en_Content_title}>{item.en.blogsAndResearch.contentTitle}</Text>
             )}
             {!showLongDesc ? (
               language === 'english' ? (
                 <Text
-                  style={{
-                    fontSize: 16,
-                    marginLeft: 15,
-                    marginTop: 10,
-                    fontFamily: 'Poppins_regular',
-                    color: 'black',
-                    lineHeight: 25,
-                  }}
+                  style={styles.Blogs_en_desc}
                 >
                   {item.en.blogsAndResearch.shortdesc}
                 </Text>
               ) : (
                 <Text
-                  style={{
-                    fontSize: 16,
-                    marginLeft: 15,
-                    marginTop: 10,
-                    fontFamily: 'Poppins_regular',
-                    color: 'black',
-                    lineHeight: 22,
-                  }}
+                  style={styles.Blogs_fr_desc}
                 >
                   {item.fr.blogsAndResearch.shortdesc}
                 </Text>
@@ -129,44 +78,24 @@ const { height, width } = Dimensions.get('window');
             ) : (
               language === 'english' ? (
                 <View style={{ flex: 1 }}>
-                  <ScrollView
-                    style={{
-                      marginLeft: 15,
-                      marginTop: 10,
-                    }}
-                  >
+                  <ScrollView>
                     <Text
-                      style={{
-                        fontSize: 16,
-                        fontFamily: 'Poppins_regular',
-                        color: 'black',
-                        lineHeight: 25,
-                      }}
+                      style={styles.Blogs_en_desc}
                     >
                       {item.en.blogsAndResearch.longdesc}
                     </Text>
-                    <Text style={{ fontSize: 20, color: '#175ca4', marginLeft: 100 }} onPress={() => { Linking.openURL(item.en.blogsAndResearch.pdf) }}>Download Pdf</Text>
+                    <Text style={styles.Blogs_Pdf} onPress={() => { Linking.openURL(item.en.blogsAndResearch.pdf) }}>Download Pdf</Text>
                   </ScrollView>
                 </View>
               ) : (
                 <View style={{ flex: 1 }}>
-                  <ScrollView
-                    style={{
-                      marginLeft: 15,
-                      marginTop: 10,
-                    }}
-                  >
+                  <ScrollView>
                     <Text
-                      style={{
-                        fontSize: 16,
-                        fontFamily: 'Poppins_regular',
-                        color: 'black',
-                        lineHeight: 22,
-                      }}
+                      style={styles.Blogs_fr_desc}
                     >
                       {item.fr.blogsAndResearch.longdesc}
                     </Text>
-                    <Text style={{ fontSize: 20, color: '#175ca4', marginLeft: 100 }} onPress={() => { Linking.openURL(item.fr.blogsAndResearch.pdf) }}>Download Pdf</Text>
+                    <Text style={styles.Blogs_Pdf} onPress={() => { Linking.openURL(item.fr.blogsAndResearch.pdf) }}>Download Pdf</Text>
                   </ScrollView>
                 </View>
               )
@@ -183,55 +112,27 @@ const { height, width } = Dimensions.get('window');
         </View>
       ))}
       <TouchableOpacity
-        onPress={() => { navigation.navigate('Login') }}
-        activeOpacity={1}
-        style={{
-          borderRadius: 50,
-          backgroundColor: '#175ca4',
-          height: 50,
-          width: '45%',
-          position: 'absolute',
-          marginTop: height - 80,
-          marginLeft: '2.5%',
-        }}
-      >
-        <Text
-          style={{
-            fontSize: 16,
-            fontFamily: 'Inter_semibold',
-            textAlign: 'center',
-            color: '#fff',
-            lineHeight: 45,
-          }}
-        >
-          LOGIN
-        </Text>
-      </TouchableOpacity>
-      <TouchableOpacity
-        activeOpacity={1}
-        onPress={()=>navigation.navigate('Enquiry')}
-        style={{
-          borderRadius: 50,
-          backgroundColor: '#175ca4',
-          height: 50,
-          width: '45%',
-          position: 'absolute',
-          marginTop: height-80,
-          marginLeft: '50%',
-        }}
-      >
-        <Text
-          style={{
-            fontSize: 16,
-            fontFamily: 'Inter_semibold',
-            textAlign: 'center',
-            color: '#fff',
-            lineHeight: 45,
-          }}
-        >
-          ENQUIRY
-        </Text>
-      </TouchableOpacity>
+              onPress={() => { navigation.navigate('Login') }}
+              activeOpacity={1}
+              style={styles.Login_Button}
+            >
+              <Text
+                style={styles.Login_Button_Text}
+              >
+                LOGIN
+              </Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              activeOpacity={1}
+              onPress={() => navigation.navigate('Enquiry')}
+              style={styles.Enquiry_Button}
+            >
+              <Text
+                style={styles.Enquiry_Button_Text}
+              >
+                ENQUIRY
+              </Text>
+            </TouchableOpacity>
     </View>
   );
 };
